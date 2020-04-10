4658d9aaf3bc20e4a496a9d6f52d16cae1b0c1bd
Karol Zurowski <zurowski.k@elettric80.it>
2020-04-09 22:44:00 -0400

    Migration do  .net core 3.1

--- FlightDeals/Startup.cs
+++ FlightDeals/Startup.cs
@@ -7,12 +7,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using FlightDeals.Services.Interfaces;
 using FlightDeals.Features;
 using AutoMapper;
 using FlightDeals.Services;
+using System.IO;
 
 namespace FlightDeals
 {
@@ -33,12 +35,17 @@ namespace FlightDeals
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
+
+
             });
 
             services.AddAutoMapper(typeof(Startup));
 
             services.AddMvc(o =>
-            o.Conventions.Add(new FeatureConvention()))
+            {
+                o.Conventions.Add(new FeatureConvention());
+                //   o.EnableEndpointRouting = false;
+            })
             .AddRazorOptions(o =>
             {
                 //{0} - action,   {1} - controller ,    {2} - area, {3} - feature
@@ -46,19 +53,22 @@ namespace FlightDeals
                 o.ViewLocationFormats.Add("/Features/{3}/{1}/{0}.cshtml");
                 o.ViewLocationFormats.Add("/Features/{3}/{0}.cshtml");
                 o.ViewLocationFormats.Add("/Features/Shared/{0}.cshtml");
-                o.ViewLocationExpanders.Add(new FeatureViewLocationExpander());
+                o.ViewLocationExpanders.Add(new FeatureViewLocationExpander());                
+
             })
-            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             services.AddHttpClient<IFlightOffersClient, FlightOffersClient>(
                 client =>
                 {
-                    client.BaseAddress = new Uri("https://test.api.amadeus.com/");   
+                    client.BaseAddress = new Uri("https://test.api.amadeus.com/");
                 });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -70,16 +80,16 @@ namespace FlightDeals
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+         
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
-            app.UseMvc(routes =>
+            app.UseRouting();
+            app.UseCors();
+            app.UseEndpoints(routes =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+               // routes.MapDefaultControllerRoute();
+                routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
